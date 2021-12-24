69fa233bf4d700a7bda94c799a08b19c3c154475
danielhands008 <danielhands008@hotmail.co.uk>
2021-12-24 14:39:44 +0000

    Added proton script selection

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 export STEAM_COMPAT_CLIENT_INSTALL_PATH="$HOME/.steam/steam"
 export STEAM_COMPAT_DATA_PATH=$(realpath "./prefix")
 #export PROTON_HIDE_NVIDIA_GPU=0
@@ -6,10 +7,39 @@ export STEAM_COMPAT_DATA_PATH=$(realpath "./prefix")
 #export PROTON_ENABLE_NVAPI=1
 mkdir -p "$STEAM_COMPAT_DATA_PATH"
 
-if [ -z "$1" ]; then
-      PROTON_COMMAND=$(zenity --width=400 --height=300 --list --title="Select Command" --column="Command" --column="Description" "exe" "Windows executable. (.exe, .msi)" "mkplay" "Make play script." "winecfg" "winecfg" "control" "Controller settings." "custom" "Custom command.")
-else
-      PROTON_COMMAND=$1
+PROTON_SCRIPT_CONFIG=./proton_script.conf
+PLAY_FILE=./play.sh
+
+select_proton_script () {
+    PROTON_SCRIPT=$(zenity --file-selection --title="Select a File" --filename="$STEAM_COMPAT_CLIENT_INSTALL_PATH/steamapps/common/" --file-filter="proton")
+    if [ -z "$PROTON_SCRIPT" ]; then
+    	zenity --width=200 --info --text="No proton version selected."
+    	exit 0
+    fi
+    echo $PROTON_SCRIPT > $PROTON_SCRIPT_CONFIG
+}
+
+if [ -f $PROTON_SCRIPT_CONFIG ]; then
+    PROTON_SCRIPT=$(<$PROTON_SCRIPT_CONFIG)
+fi
+
+if [ -z "$PROTON_SCRIPT" ]; then
+    zenity --width=200 --info --text="Select proton script."
+    select_proton_script
+fi
+
+select_command () {
+  if [ -z "$1" ]; then
+        PROTON_COMMAND=$(zenity --width=400 --height=300 --list --title="Select Command" --column="Command" --column="Description" "selectproton" "Select proton version." "exe" "Windows executable. (.exe, .msi)" "mkplay" "Make play script." "winecfg" "winecfg" "control" "Controller settings." "custom" "Custom command.")
+  else
+        PROTON_COMMAND=$1
+  fi
+}
+select_command
+
+if [ "$PROTON_COMMAND" == "selectproton" ]; then
+    select_proton_script
+    select_command
 fi
 
 if [ "$PROTON_COMMAND" == "exe" ]; then
@@ -22,12 +52,16 @@ if [ "$PROTON_COMMAND" == "mkplay" ]; then
 
 PROTON_COMMAND=
 
-PLAY_FILE=./play.sh
-
 zenity --width=200 --info --text="Select game install folder."
-GAME_DIR=$(zenity --file-selection --directory --filename="./")
+GAME_DIR=$(zenity --file-selection --directory --filename="$(realpath ./)/")
+if [ -z "$GAME_DIR" ]; then
+    exit 0
+fi
 zenity --width=200 --info --text="Select game exe."
 GAME_EXE=$(zenity --file-selection --filename="$GAME_DIR/")
+if [ -z "$GAME_EXE" ]; then
+    exit 0
+fi
 
 cat > $PLAY_FILE <<- EOM
 #!/bin/bash
@@ -38,13 +72,15 @@ export STEAM_COMPAT_DATA_PATH=\$(realpath "./prefix")
 #export PROTON_ENABLE_NVAPI=1
 mkdir "\$STEAM_COMPAT_DATA_PATH"
 cd "$GAME_DIR"
-/mnt/linux-games/Steam/steamapps/common/Proton\ -\ Experimental/proton run "$GAME_EXE" 
+"$PROTON_SCRIPT" run "$GAME_EXE" 
 EOM
 
 chmod +x $PLAY_FILE
 
+select_command
+
 fi
 
 if [ -n "$PROTON_COMMAND" ]; then
-    /mnt/linux-games/Steam/steamapps/common/Proton\ -\ Experimental/proton run "$PROTON_COMMAND"
+    "$PROTON_SCRIPT" run "$PROTON_COMMAND"
 fi
