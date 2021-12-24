a5a3df97c91897de576196de08aba59f7437896f
danielhands008 <danielhands008@hotmail.co.uk>
2021-12-24 15:18:25 +0000

    Added .desktop creation

--- run.sh
+++ run.sh
@@ -11,16 +11,19 @@ PROTON_SCRIPT_CONFIG=./proton_script.conf
 PLAY_FILE=./play.sh
 
 select_proton_script () {
-    PROTON_SCRIPT=$(zenity --file-selection --title="Select a File" --filename="$STEAM_COMPAT_CLIENT_INSTALL_PATH/steamapps/common/" --file-filter="proton")
+    PROTON_SCRIPT=$(zenity --file-selection --title="Select proton script" --filename="$STEAM_COMPAT_CLIENT_INSTALL_PATH/steamapps/common/" --file-filter="proton")
     if [ -z "$PROTON_SCRIPT" ]; then
     	zenity --width=200 --info --text="No proton version selected."
     	exit 0
     fi
-    echo $PROTON_SCRIPT > $PROTON_SCRIPT_CONFIG
+    echo $PROTON_SCRIPT > "$PROTON_SCRIPT_CONFIG"
 }
 
-if [ -f $PROTON_SCRIPT_CONFIG ]; then
-    PROTON_SCRIPT=$(<$PROTON_SCRIPT_CONFIG)
+if [ -f "$PROTON_SCRIPT_CONFIG" ]; then
+    PROTON_SCRIPT=$(<"$PROTON_SCRIPT_CONFIG")
+    if [ ! -f "$PROTON_SCRIPT" ]; then
+        select_proton_script
+    fi
 fi
 
 if [ -z "$PROTON_SCRIPT" ]; then
@@ -30,7 +33,7 @@ fi
 
 select_command () {
   if [ -z "$1" ]; then
-        PROTON_COMMAND=$(zenity --width=400 --height=300 --list --title="Select Command" --column="Command" --column="Description" "selectproton" "Select proton version." "exe" "Windows executable. (.exe, .msi)" "mkplay" "Make play script." "winecfg" "winecfg" "control" "Controller settings." "custom" "Custom command.")
+        PROTON_COMMAND=$(zenity --width=400 --height=300 --list --title="Select Command" --column="Command" --column="Description" "selectproton" "Select proton version." "exe" "Windows executable. (.exe, .msi)" "mkplay" "Make play script." "mkdesktop" "Add to launcher. (Requires play script.)" "winecfg" "winecfg" "control" "Controller settings." "custom" "Custom command.")
   else
         PROTON_COMMAND=$1
   fi
@@ -53,17 +56,17 @@ if [ "$PROTON_COMMAND" == "mkplay" ]; then
 PROTON_COMMAND=
 
 zenity --width=200 --info --text="Select game install folder."
-GAME_DIR=$(zenity --file-selection --directory --filename="$(realpath ./)/")
+GAME_DIR=$(zenity --file-selection --title="Select game install folder." --directory --filename="$(realpath ./)/")
 if [ -z "$GAME_DIR" ]; then
     exit 0
 fi
 zenity --width=200 --info --text="Select game exe."
-GAME_EXE=$(zenity --file-selection --filename="$GAME_DIR/")
+GAME_EXE=$(zenity --file-selection --title="Select game exe." --filename="$GAME_DIR/")
 if [ -z "$GAME_EXE" ]; then
     exit 0
 fi
 
-cat > $PLAY_FILE <<- EOM
+cat > "$PLAY_FILE" <<- EOM
 #!/bin/bash
 export STEAM_COMPAT_CLIENT_INSTALL_PATH="\$HOME/.steam/steam"
 export STEAM_COMPAT_DATA_PATH=\$(realpath "./prefix")
@@ -75,10 +78,40 @@ cd "$GAME_DIR"
 "$PROTON_SCRIPT" run "$GAME_EXE" 
 EOM
 
-chmod +x $PLAY_FILE
+chmod +x "$PLAY_FILE"
 
 select_command
 
+fi
+
+if [ "$PROTON_COMMAND" == "mkdesktop" ]; then
+DESKTOP_NAME=$(zenity --entry --text="Name")
+if [ -z "$DESKTOP_NAME" ]; then
+    exit 0
+fi
+ICON_FILE=$(zenity --file-selection --title="Select an Icon (Optional)" --file-filter=""*.png" "*.jpg" "*.jpeg" "*.svg"")
+DESKTOP_FILE=$(zenity --file-selection --save --title="Save Desktop File" --filename="$(realpath ~/.local/share/applications/)/Game.desktop" --file-filter="*.desktop")
+if [ -z "$DESKTOP_FILE" ]; then
+    exit 0
+fi
+
+cat > "$DESKTOP_FILE" <<- EOM
+[Desktop Entry]
+Version=1.1
+Type=Application
+Name=$DESKTOP_NAME
+Icon=/home/daniel/Games/gog/Tokyo Xanadu eX/icon.png
+Exec=bash -c 'cd "$(realpath ./)/" && ./play.sh'
+Actions=
+Categories=X-GNOME-Other;
+Actions=settings;
+
+[Desktop Action settings]
+Name=Proton Commands
+Exec=bash -c 'cd "$(realpath ./)/" && ./run.sh'
+Icon=/home/daniel/Games/gog/Tokyo Xanadu eX/icon.png
+EOM
+select_command
 fi
 
 if [ -n "$PROTON_COMMAND" ]; then
