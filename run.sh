cfa9548ff1a39779e04c328832051b6243ff2fc4
danielhands008 <danielhands008@hotmail.co.uk>
2022-01-11 05:02:19 +0000

    Added comments and chenged settings file.

--- run.sh
+++ run.sh
@@ -11,9 +11,24 @@ if [ ! -f "$STEAM_COMPAT_DATA_PATH/tracked_files" ]; then
     echo " " > "$STEAM_COMPAT_DATA_PATH/tracked_files"
 fi
 
-
+CONFIG_FILE=./proton_config.conf
 PROTON_SCRIPT_CONFIG=./proton_script.conf
 PLAY_FILE=./play.sh
+
+load_env_values () {
+    while IFS= read -r line
+    do
+        if [[ "$line" == *"="* ]]; then
+            IFS='=' read -r env value <<< "$line"
+            export $env="$value"
+        fi
+    done < "$CONFIG_FILE"
+}
+
+save_env_values () {
+    # First env use a single > to clear the file, then us double >> for each after.
+    echo PROTON_SCRIPT=$PROTON_SCRIPT > "$CONFIG_FILE"
+}
 
 select_proton_script () {
     PROTON_SCRIPT=$(zenity --file-selection --title="Select proton script" --filename="$STEAM_COMPAT_CLIENT_INSTALL_PATH/steamapps/common/" --file-filter="proton")
@@ -21,42 +36,45 @@ select_proton_script () {
         zenity --width=200 --info --text="No proton version selected."
         exit 0
     fi
-    echo $PROTON_SCRIPT > "$PROTON_SCRIPT_CONFIG"
+    save_env_values
 }
 
-if [ -f "$PROTON_SCRIPT_CONFIG" ]; then
-    PROTON_SCRIPT=$(<"$PROTON_SCRIPT_CONFIG")
-    if [ ! -f "$PROTON_SCRIPT" ]; then
-        select_proton_script
-    fi
-fi
+select_command () {
+    PROTON_COMMAND=$(zenity --width=400 --height=300 --list --title="Select Command" --column="Command" --column="Description" "selectproton" "Select proton version." "exe" "Windows executable. (.exe, .msi)" "winetricks" "Open winetricks." "mkplay" "Make play script." "mkdesktop" "Add to launcher. (Requires play script.)" "winecfg" "winecfg" "control" "Controller settings." "custom" "Custom command.")
+}
 
+
+load_env_values
+
+# Select proton version, required before any other action.
 if [ -z "$PROTON_SCRIPT" ]; then
     zenity --width=200 --info --text="Select proton script."
     select_proton_script
 fi
 
-select_command () {    
-    PROTON_COMMAND=$(zenity --width=400 --height=300 --list --title="Select Command" --column="Command" --column="Description" "selectproton" "Select proton version." "exe" "Windows executable. (.exe, .msi)" "winetricks" "Open winetricks." "mkplay" "Make play script." "mkdesktop" "Add to launcher. (Requires play script.)" "winecfg" "winecfg" "control" "Controller settings." "custom" "Custom command.")
-}
+# TODO: Check if proton script from $PROTON_SCRIPT exists.
 
+# If no arguments were specified then show command selector.
 if [ -z "$1" ]; then
     select_command
 else
     PROTON_COMMAND=$1
 fi
 
+# Select proton version.
 if [ "$PROTON_COMMAND" == "selectproton" ]; then
     select_proton_script
     select_command
 fi
 
+# Run a windows exe in the prefix.
 if [ "$PROTON_COMMAND" == "exe" ]; then
     PROTON_COMMAND=$(zenity --file-selection --title="Select a File" --file-filter=""*.exe" "*.msi" "*.EXE" "*.MSI"")
     elif [ "$PROTON_COMMAND" == "custom" ]; then
     PROTON_COMMAND=$(zenity --entry --text="Command")
 fi
 
+# Make play script.
 if [ "$PROTON_COMMAND" == "mkplay" ]; then
     
     PROTON_COMMAND=
@@ -82,6 +100,7 @@ export STEAM_COMPAT_DATA_PATH=\$(realpath "./prefix")
 mkdir "\$STEAM_COMPAT_DATA_PATH"
 cd "$GAME_DIR"
 "$PROTON_SCRIPT" run "$GAME_EXE"
+read -p "Keep this window open while game is running."
 EOM
     
     chmod +x "$PLAY_FILE"
@@ -90,6 +109,7 @@ EOM
     
 fi
 
+# Make desktop file.
 if [ "$PROTON_COMMAND" == "mkdesktop" ]; then
     DESKTOP_NAME=$(zenity --entry --text="Name")
     if [ -z "$DESKTOP_NAME" ]; then
@@ -120,15 +140,17 @@ EOM
     select_command
 fi
 
+# Open winetricks in the current prefix.
 if [ "$PROTON_COMMAND" == "winetricks" ]; then
-
-export WINE=$(dirname "$PROTON_SCRIPT")/files/bin/wine64
-export LD_LIBRARY_PATH=$(dirname "$PROTON_SCRIPT")/files/lib:$LD_LIBRARY/PATH
-export WINEPREFIX="$STEAM_COMPAT_DATA_PATH/pfx"
-PROTON_COMMAND=
-winetricks --gui
+    export WINE=$(dirname "$PROTON_SCRIPT")/files/bin/wine64
+    export LD_LIBRARY_PATH=$(dirname "$PROTON_SCRIPT")/files/lib:$LD_LIBRARY/PATH
+    export WINEPREFIX="$STEAM_COMPAT_DATA_PATH/pfx"
+    PROTON_COMMAND=
+    winetricks --gui
+    select_command
 fi
 
+# Otherwise run the value of $PROTON_COMMAND in proton.
 if [ -n "$PROTON_COMMAND" ]; then
     "$PROTON_SCRIPT" run "$PROTON_COMMAND"
 fi
